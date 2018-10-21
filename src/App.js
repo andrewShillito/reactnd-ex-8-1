9ef1fde288e789e82f557384228b7e4c7d418222
andrewShillito <andrew.shillito88@gmail.com>
2018-10-21 18:04:08 +0000

    working version

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    query: "",
+  }
+  updateQuery = (query) => {
+    this.setState(() => ({
+      query: query,
+    }))
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">ReactND - Coding Practice</h1>
-        </header>
+        
+        //add header here
+        
         <div className="container">
-          <input type="text" placeholder="Say Something" />
+          <input 
+            type="text" 
+            placeholder="Say Something" 
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+            />
           <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <p>{this.state.query}</p>
         </div>
       </div>
     );
